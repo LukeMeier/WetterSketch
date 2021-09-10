1f9fb1b24dfa09ef954a044fd06fd41d80f00b52
Luke Meier <luke.meier1@gmail.com>
2021-09-10 10:37:19 +0200

    Cloud function hinzugefügt

--- Wetter_Projekt_LukeMeier/Wetter_Projekt_LukeMeier.js
+++ Wetter_Projekt_LukeMeier/Wetter_Projekt_LukeMeier.js
@@ -1,8 +1,20 @@
 function setup() {
-
+  createCanvas(windowWidth, windowHeight);
+  background(100);
 }
 
 
-function draw() {
 
+function draw() {
+  cloud();
+}
+
+function cloud(x, y){
+  fill( 255, 255, 255);
+  stroke(255,255,255);
+  ellipse(x-30, y+10, 50);
+  ellipse(x, y, 70);
+  ellipse(x+30, y+2, 65);
+  ellipse(x+50, y+10, 50);
+  
 }
